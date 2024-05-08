7a67f52162364a69ba8fc91fec9c3bbd0618e5bb
bestorbust <155546102+bestorbust@users.noreply.github.com>
2024-05-09 01:39:48 +0530

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 def get_db_connection():
     conn = sqlite3.connect('hospital.db')
